9cf6eca604ce79a60353553c8702318604c7c8a6
gardoffwhite <gardoffwhite@gmail.com>
2025-04-09 16:23:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('Gacha Backend')); app.listen(3000);
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;  // Render จะใช้พอร์ตนี้โดยอัตโนมัติ
+
+app.get('/', (req, res) => {
+  res.send('Gacha Backend');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
