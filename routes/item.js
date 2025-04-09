03192ab30ca9bad816717b12dfb68000e57ba3e6
gardoffwhite <gardoffwhite@gmail.com>
2025-04-09 16:06:39 +0700

    Delete routes directory

--- routes/item.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { randomItem } = require('../controllers/itemController');
-const router = express.Router();
-
-router.post('/random', randomItem);
-
-module.exports = router;
