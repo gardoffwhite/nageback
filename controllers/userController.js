f2231cb796b58241148e1ab73fd5726a47345867
gardoffwhite <gardoffwhite@gmail.com>
2025-04-09 15:28:22 +0700

    Create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.signup = async (req, res) => {
+    // Logic for user signup
+};
+
+exports.login = async (req, res) => {
+    // Logic for user login
+};
